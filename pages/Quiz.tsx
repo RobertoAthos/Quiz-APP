0c261e06878ea57a825a63857aa32c0c364e2f5e
Roberto Athos <robertoathos22@gmail.com>
2022-11-17 15:31:55 -0300

    modal answers

--- pages/Quiz.tsx
+++ pages/Quiz.tsx
@@ -10,7 +10,10 @@ import {
   AiOutlineClose,
 } from "react-icons/ai";
 import Transition from "../Components/Transition";
-import Link from "next/link";
+import Link from "next/link"
+import Image from 'next/image'
+import img from '../Assets/thumbsup.png'
+import {BiSad} from 'react-icons/bi'
 
 export default function Quiz() {
   const [currentQuestion, setCurrentQuestion] = useState(0);
@@ -68,7 +71,16 @@ export default function Quiz() {
         {correctAnswer == true ? (
           <div className=" w-full h-screen bg-[#000000a9] absolute flex justify-center items-center">
             <Transition direction="right">
-              <ModalCorrectAnswer />
+            <div className='bg-[#4CFFA5] w-[500px] h-[400px] rounded-3xl text-center shadow-2xl flex flex-col justify-center items-center'>
+        <h3 className='text-3xl text-white font-bold mt-6'>Parabéns !! <br/> Resposta Correta</h3>
+        <picture>
+            <Image
+              src={img}
+              alt="thumbsup image"
+              className='w-80'
+            />
+        </picture>
+    </div>
             </Transition>
             <button
                 onClick={() => setCorrectAnswer(false)}
@@ -83,7 +95,12 @@ export default function Quiz() {
         {incorrectAnswer == true ? (
           <div className=" w-full h-screen bg-[#000000a9] absolute flex justify-center items-center">
             <Transition direction="right">
-              <ModalIncorrectAswer />
+            <div className='bg-[#f02727] w-[500px] h-[400px] rounded-3xl text-center shadow-2xl flex flex-col justify-center items-center'>
+        <h3 className='text-3xl text-white font-bold mt-6'>Que Pena !! <br/> Você Errou</h3>
+        <picture className='mt-8'>
+            <BiSad className='text-white text-9xl'/>
+        </picture>
+    </div>
             </Transition>
             <button
                 onClick={() => setIncorrectAnswer(false)}
